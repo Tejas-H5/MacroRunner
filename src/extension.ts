ea5d78f5612ed1a29c01e8b365da43363cd87257
El-Tejaso <45031546+El-Tejaso@users.noreply.github.com>
2022-03-26 12:04:34 +1030

    Improved flow, refactored code

--- src/extension.ts
+++ src/extension.ts
@@ -1,10 +1,6 @@
 import * as vscode from "vscode";
-import DebugContext from "./debugContext";
-import { createIntervalTimeoutFunctions } from "./intervalTimeout";
-import MacroContext from "./macroContext";
 import macroTemplate from "./macroTemplate";
-import { replaceAllFile, showErrors } from "./macroUtil";
-import { containsWhileLoop } from "./sourceUtil";
+import { runMacroCommand } from "./runMacroCommand";
 
 const newMacroCommand = async () => {
     let document = await vscode.workspace.openTextDocument({
@@ -17,95 +13,6 @@ const newMacroCommand = async () => {
         document,
         visibleEditors.length === 1 ? vscode.ViewColumn.Beside : vscode.ViewColumn.Active
     );
-};
-
-const runMacroCommand = async () => {
-    let visibleEditors = vscode.window.visibleTextEditors;
-
-    if (visibleEditors.length !== 2 || !visibleEditors[0].document || !visibleEditors[1].document) {
-        vscode.window
-            .showErrorMessage(`This command needs exactly two editors to be open in a split configuration 
-        - one with the javascript macro and another with the target document. Create a new macro with the New macro command`);
-        return;
-    }
-
-    let activeEditor = vscode.window.activeTextEditor;
-    if (!activeEditor) {
-        vscode.window.showErrorMessage(`The macro editor must have focus for this command to run`);
-        return;
-    }
-
-    let macroDocument = activeEditor.document;
-    if (macroDocument.languageId !== "javascript") {
-        vscode.window.showErrorMessage(`Error`, {
-            detail: "The macro window must have JavaScript code. Are you sure this is the correct window?",
-        });
-        return;
-    }
-
-    let code = macroDocument.getText();
-    if (!code.substring(0, code.indexOf("\n")).toLowerCase().includes("macro")) {
-        vscode.window.showErrorMessage(
-            `The first line of the macro must contain the word 'macro' somewhere in it, possibly in a comment. Are you sure this is the correct window?`
-        );
-        return;
-    }
-
-    if (containsWhileLoop(code)) {
-        // wait for the user to close the warning before we proceed
-        await vscode.window.showWarningMessage(
-            `I strongly recommend against using while loops in your code, as this extension has no way to break out infinite loops at the moment. 
-If you aren't very sure that this code won't hang, ready up a Task Manager or command shell to kill this process and then close this popup.`,
-            { modal: true }
-        );
-    }
-
-    // prepare objects to feed to the macro
-    const targetEditorIndex = activeEditor === visibleEditors[0] ? 1 : 0;
-    const targetEditor = visibleEditors[targetEditorIndex];
-    const targetEditorLanguage = targetEditor.document.languageId;
-    const initialDocument = targetEditor.document;
-    const ctx = new MacroContext(targetEditor);
-    const debug = new DebugContext();
-    const timerContainer = createIntervalTimeoutFunctions();
-
-    const allInjectedFunctions = [...timerContainer.functions];
-
-    // actually run the macro
-    try {
-        const macroFunction = Function(`
-            "use strict";
-            return (async (context, debug, ${allInjectedFunctions.map((o) => o.name).join(",")}) => {
-                ${code}
-            });`)();
-        await macroFunction(ctx, debug, ...allInjectedFunctions);
-    } catch (e: any) {
-        showErrors(e);
-        return;
-    }
-
-    // wait for all the async stuff to finish, so that the code there is still able to throw exceptions
-    await timerContainer.joinAll();
-
-    //apply all changes that need to be applied
-
-    // update the current text file if we had any outputs
-    const newFile = ctx.getFile(0);
-
-    if (newFile.getText() !== "") {
-        await replaceAllFile(newFile, initialDocument, targetEditor.viewColumn, true);
-    }
-
-    // create and update all non-empty output files
-    for (let i = 1; i < ctx.fileCount(); i++) {
-        if (ctx.getFile(i).getText() === "") continue;
-        let newDocument = await vscode.workspace.openTextDocument({
-            content: "",
-            language: targetEditorLanguage,
-        });
-
-        await replaceAllFile(ctx.getFile(i), newDocument, targetEditor.viewColumn, true);
-    }
 };
 
 export function activate(context: vscode.ExtensionContext) {
---
+++ src/runMacroCommand.ts
@@ -0,0 +1,132 @@
+import * as vscode from "vscode";
+import DebugContext from "./debugContext";
+import MacroContext from "./macroContext";
+import { createIntervalTimeoutFunctions } from "./intervalTimeout";
+import { replaceAllFile, showErrors } from "./macroUtil";
+import { containsWhileLoop } from "./sourceUtil";
+
+export const runMacroCommand = async () => {
+    try {
+        const macroEditor = getEditorWithMacroFile();
+        const targetEditor = getEditorWithTargetFile(macroEditor);
+
+        const executionResult = await runMacro(macroEditor, targetEditor);
+
+        if (!executionResult) {
+            return;
+        }
+
+        await applyMacroContextResult(executionResult, targetEditor);
+    } catch (err: any) {
+        vscode.window.showErrorMessage(err.message);
+    }
+};
+
+const getEditorWithMacroFile = () => {
+    let visibleEditors = vscode.window.visibleTextEditors;
+    let macroEditors = visibleEditors.filter((editor) => {
+        const code = editor.document.getText();
+        const containsSafetyCatch = code.substring(0, code.indexOf("\n")).toLowerCase().includes("macro");
+        return containsSafetyCatch;
+    });
+
+    if (macroEditors.length === 0) {
+        throw new Error(
+            "Make sure your macro has the word 'macro' somewhere on the first line (this is a safety catch), or that you have the right file visible"
+        );
+    }
+
+    if (macroEditors.length > 1) {
+        throw new Error("Found multiple macros, make sure that only the macro you want to run is visible.");
+    }
+
+    // sometimes copy-pasted macro code can resolve to c/c++
+    if (macroEditors[0].document.languageId !== "javascript") {
+        vscode.languages.setTextDocumentLanguage(macroEditors[0].document, "javascript");
+    }
+
+    return macroEditors[0];
+};
+
+const getEditorWithTargetFile = (macroEditor: vscode.TextEditor) => {
+    let editor = vscode.window.activeTextEditor;
+    if (editor && editor !== macroEditor) return editor;
+
+    const visibleEditors = vscode.window.visibleTextEditors;
+    let visibleTextEditors = new Array<vscode.TextEditor>();
+
+    for (const visibleEditor of visibleEditors) {
+        // todo: check if editor is actually readonly
+        if (true) {
+            visibleTextEditors.push(visibleEditor);
+        }
+    }
+
+    if (visibleEditors.length === 2 || editor === undefined) {
+        return visibleEditors[0] === macroEditor ? visibleEditors[1] : visibleEditors[0];
+    } else {
+        throw new Error(
+            "When you have more than two other editors open, bring focus to the one you want to run the macro in"
+        );
+    }
+};
+
+const runMacro = async (macroEditor: vscode.TextEditor, targetEditor: vscode.TextEditor) => {
+    const code = macroEditor.document.getText();
+    if (containsWhileLoop(code)) {
+        // wait for the user to close the warning before proceeding anyway
+        await vscode.window.showWarningMessage(
+            `I strongly recommend against using while loops in your code, as this extension has no way to break out infinite loops at the moment. 
+If you aren't very sure that this code won't hang, ready up a Task Manager or command shell to kill this process and then close this popup.`,
+            { modal: true }
+        );
+    }
+
+    // prepare objects to feed to the macro
+    const ctx = new MacroContext(targetEditor);
+    const debug = new DebugContext();
+    const timerContainer = createIntervalTimeoutFunctions();
+
+    const allInjectedFunctions = [...timerContainer.functions];
+
+    // actually run the macro
+    try {
+        const macroFunction = Function(`
+          "use strict";
+          return (async (context, debug, ${allInjectedFunctions.map((o) => o.name).join(",")}) => {
+              ${code}
+          });`)();
+        await macroFunction(ctx, debug, ...allInjectedFunctions);
+    } catch (e: any) {
+        showErrors(e);
+        return;
+    }
+
+    // await all the timers to finish, so that the code there still has access to the vscode debugger and can
+    // create error modals
+    await timerContainer.joinAll();
+
+    return ctx;
+};
+
+const applyMacroContextResult = async (ctx: MacroContext, targetEditor: vscode.TextEditor) => {
+    // update the current text file if we had any outputs
+    const targetEditorLanguage = targetEditor.document.languageId;
+    const initialDocument = targetEditor.document;
+    const newFile = ctx.getFile(0);
+
+    if (newFile.getText() !== "") {
+        await replaceAllFile(newFile, initialDocument, targetEditor.viewColumn, true);
+    }
+
+    // create and update all non-empty output files
+    for (let i = 1; i < ctx.fileCount(); i++) {
+        if (ctx.getFile(i).getText() === "") continue;
+        let newDocument = await vscode.workspace.openTextDocument({
+            content: "",
+            language: targetEditorLanguage,
+        });
+
+        await replaceAllFile(ctx.getFile(i), newDocument, targetEditor.viewColumn, true);
+    }
+};
