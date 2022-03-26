9ce7204d844882a0e37258dbefcfa24223ba62c6
El-Tejaso <45031546+El-Tejaso@users.noreply.github.com>
2022-03-26 21:55:33 +1030

    Added unit tests for while loop detection

--- src/sourceUtil.ts
+++ src/sourceUtil.ts
@@ -1,4 +1,5 @@
 // just a heuristic, wont work in all cases since we aren't doing any parsing
+// also doesn't detect for(;;). people who use for(;;) are truly deranged and don't need saving
 export const containsWhileLoop = (src: string) => {
     // delete all strings (cheers https://stackoverflow.com/questions/49906179/regex-to-match-string-syntax-in-code)
     src.replace(/(`|"|')(?:(?!\2)(?:\\.|[^\\]))*\2/gs, "");
@@ -10,5 +11,5 @@ export const containsWhileLoop = (src: string) => {
     src.replace(/\/\/.+/g, "");
 
     // do we still have while(something) in our code?
-    return !!src.match(/while\(.+\)/);
+    return !!src.match(/[^\S]while\(.+\)/s);
 };
---
+++ src/test/suite/sourceutil.test.ts
@@ -0,0 +1,72 @@
+import * as assert from "assert";
+import * as vscode from "vscode";
+import * as macroRunner from "../../extension";
+import { containsWhileLoop } from "../../sourceUtil";
+
+suite("containsWhileLoop", () => {
+    test("while loop", () => {
+        assert(
+            containsWhileLoop(`        
+while(array[4][5][6][7].eightySix[99][\`bruh\`] === "xd") {
+
+} 
+        `)
+        );
+    });
+
+    test("while loop 2", () => {
+        assert(
+            containsWhileLoop(`        
+            while(true.x.y.z) {
+
+            } 
+        `)
+        );
+    });
+
+    test("ignore regex", () => {
+        assert(
+            !containsWhileLoop(`
+        /while while hwile while while/
+        
+        /while/
+        `)
+        );
+    });
+
+    test("ignore strings", () => {
+        assert(
+            !containsWhileLoop(`
+        'while while hwile while while'
+        "while while hwile while while"        
+        \`while while hwile while while\`
+        `)
+        );
+    });
+
+    test("ignore comments", () => {
+        assert(
+            !containsWhileLoop(`
+        // while while hwile while while
+        /*
+        * while while hwile while while
+        * while while hwile while while
+        * ** ";\\\`  */"        
+        `)
+        );
+    });
+
+    test("ignore variable names", () => {
+        assert(
+            !containsWhileLoop(`
+        let whileStillSearching = true;
+        let shouldDoWhile = () => {
+
+        }
+        let function dowhile(x) {
+
+        }
+        `)
+        );
+    });
+});
